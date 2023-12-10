2f9eed7aa6781ba14196441b6e2db797d18a7633
zumassh <stasy.sl@yandex.ru>
2023-12-10 16:41:42 +0300

    adaptation fix

--- main.js
+++ main.js
@@ -5,12 +5,23 @@ function show(){
         attempt = 1;
         let spis = document.querySelector(".typeList");
         spis.style.opacity = "1";
+        spis.style.display = "flex";
     }
 
     else if (attempt === 1){
         attempt = 0;
         let spis = document.querySelector(".typeList");
         spis.style.opacity = "0";
+        spis.style.display = "none";
     }
 }
 
+document.addEventListener('DOMContentLoaded',function(event){
+    let spis = document.querySelector(".typeList");
+    let button = document.querySelector('.menu');
+    let nav  = document.querySelector('nav');
+    button.addEventListener('click',function(event){
+        button.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+});
